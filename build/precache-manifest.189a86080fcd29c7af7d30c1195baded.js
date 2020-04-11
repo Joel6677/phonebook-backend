2b920a295c134bc051f204be270719510b7bbe7c
Joel6677 <joel.heusala@gmail.com>
2020-04-11 04:31:07 +0300

    build

---
+++ build/precache-manifest.189a86080fcd29c7af7d30c1195baded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6983f4e2831b77d4c6efc3c4540b0aaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1247bc349fb638bb04e3",
+    "url": "/static/css/main.c154416a.chunk.css"
+  },
+  {
+    "revision": "5181c4358cb023025e8d",
+    "url": "/static/js/2.4cd51178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cd51178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1247bc349fb638bb04e3",
+    "url": "/static/js/main.a096d645.chunk.js"
+  },
+  {
+    "revision": "557cbf34b3689c032070",
+    "url": "/static/js/runtime-main.7dc14107.js"
+  }
+]);
